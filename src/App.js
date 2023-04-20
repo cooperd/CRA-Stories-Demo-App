d02685975257a461569125e5e567823a2b0a802e
Dvir Cooepr <cooperdvir@gmail.com>
2023-04-20 22:08:23 +0300

    adding production build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from "./components/AppBar";
+import Stories from "./components/Stories";
+import AppBottomNavigation from "./components/AppBottomNevigation";
+import Container from "@mui/material/Container";
+import {Box, CssBaseline} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              minHeight: '100vh',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <CssBaseline />
+        <ResponsiveAppBar/>
+        <Container component="main" maxWidth="sm" sx={{ px: 0 }} >
+          <Stories/>
+        </Container>
+          <Box
+              component="footer"
+              sx={{
+                  position: 'fixed', bottom: 0, left: 0, right: 0,
+                  px:0,
+                  mt: 'auto',
+                  backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[800],
+              }}
+          >
+              <Container>
+        <AppBottomNavigation/>
+              </Container>
+          </Box>
+    </Box>
   );
 }
 
